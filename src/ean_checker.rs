974ee69dc4249b4432edb555a9d9abb78568c626
VERCHERE Rémi <remi.verchere2@gmail.com>
2024-01-13 19:35:22 +0100

    parse code in files

---
+++ src/ean_checker.rs
@@ -0,0 +1,10 @@
+
+
+pub fn is_correct_ean() {
+    println!("Correct EAN!");
+    calculate_digit_check();
+}
+
+fn calculate_digit_check() {
+    println!("Correct digit check!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+
+mod ean_checker;
+
 fn main() {
     println!("Hello, world!");
+    ean_checker::is_correct_ean();
 }
