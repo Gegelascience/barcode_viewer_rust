5bfae6c69c46abe5a314fb46af3a64573b9286c1
VERCHERE Rémi <remi.verchere2@gmail.com>
2024-01-13 23:27:18 +0100

    check ean ok

--- src/ean_checker.rs
+++ src/ean_checker.rs
@@ -1,10 +1,66 @@
 
 
-pub fn is_correct_ean(ean_test: &str) {
+pub fn is_correct_ean(ean_test: &str) -> bool {
+    
+    // check length
+
+    let test_length = ean_test.len();
+    if test_length != 13 && test_length != 8 {
+      return false;  
+    }
+
+    for c in ean_test.chars() {
+        if !c.is_numeric() {
+            return false;
+        }
+    }
+
+    // calculate digit
+
+    let len_withoutdigit = test_length-1;
+    println!("{}",&ean_test[..len_withoutdigit]);
+    let digit = calculate_digit_check(&ean_test[..len_withoutdigit]);
+
+
+    println!("{}",ean_test[..test_length].chars().last().unwrap().to_digit(10).unwrap());
+    println!("{}",digit);
+
+    println!("{}",ean_test[..test_length].chars().last().unwrap().to_digit(10).unwrap() == digit);
+    
+    
+    if ean_test[..test_length].chars().last().unwrap().to_digit(10).unwrap() != digit {
+        return false;
+    }
+
     println!("Correct EAN! {}", ean_test);
-    calculate_digit_check();
+
+    return true;
 }
 
-fn calculate_digit_check() {
-    println!("Correct digit check!");
+fn calculate_digit_check(ean_digit_check_less:&str) -> u32 {
+    
+
+    let test_length = ean_digit_check_less.len();
+    let mut factor = 3;
+    let mut somme = 0;
+
+    for c in ean_digit_check_less.chars() {
+        if !c.is_numeric() {
+            panic!("Incorrect characters");
+        }
+    }
+
+    let mut i: i32 = test_length as i32 -1;
+    let step: i32 = -1;
+    let end: i32 = -1;
+
+    while i > end {
+        somme += ean_digit_check_less.chars().nth(i.try_into().unwrap()).unwrap().to_digit(10).unwrap() * factor;
+        factor = 4 - factor;
+        i += step;
+    }
+
+    let digit_check = (10 - (somme % 10))%10;
+
+    return digit_check;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod ean_checker;
 
 fn main() {
     println!("Hello, world!");
-    ean_checker::is_correct_ean("3666154117284");
+    let result = ean_checker::is_correct_ean("3666154117284");
+    println!("{}",result)
 }
