f6645511543b53fd2f3799eb753aa4360386dea9
VERCHERE Rémi <remi.verchere2@gmail.com>
2024-02-03 18:39:28 +0100

    ecriture png OK

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod ean_checker;
 mod barcode_data;
 mod svg_parser;
+mod png_writer;
 
 use std::{fs,io};
 
@@ -31,9 +32,63 @@ fn main() -> std::io::Result<()> {
         let barcode_data = barcode_data::calculate_barcode_ean13(&ean_test);
         println!("{}",barcode_data);
 
-        let svg_content =  svg_parser::get_svg_string(barcode_data);
+        let svg_content =  svg_parser::get_svg_string(barcode_data.clone());
         
         fs::write("test.svg", svg_content.as_bytes())?;
+
+        let mut f = std::fs::File::create("test.png").unwrap();
+
+        let image_width = 95 *10;
+        let image_height = 50;
+        let mut img_data: Vec<u8> = Vec::new();
+        let mut line_index = 0;
+
+        while line_index < image_height {
+            // ecriture d une ligne
+            for char in barcode_data.chars() {
+                if char == '1' {
+                    for _n in 0..10 {
+                        img_data.push(
+                            0x00
+                        );
+                        img_data.push(
+                            0x00
+                        );
+                        img_data.push(
+                            0x00
+                        );
+                        img_data.push(
+                            0xff
+                        )
+                    }
+                
+                } else {
+                    for _n in 0..10 {
+                        img_data.push(
+                            0xff
+                        );
+                        img_data.push(
+                            0xff
+                        );
+                        img_data.push(
+                            0xff
+                        );
+                        img_data.push(
+                            0xff
+                        )
+                    }
+                
+                }
+    
+            }
+
+            line_index +=1
+        }
+
+        match png_writer::write(&mut f, &img_data, image_width, image_height) {
+            Ok(_) => println!("Written image!"),
+            Err(e) => println!("Error {:?}", e),
+        }
     }
 
     Ok(())
